782ae3fd514d52f41c985bd78e3417ab7632d2ed
Sergei <qaserge@gmail.com>
2024-01-22 14:03:43 -0500

    useState added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
+import { useState } from "react";
+
 const message = ["step 1", "step 2", "step 3"];
 
 export default function App() {
-  const step = 1;
+  const [step, setStep] = useState(1);
 
   function handleBackStep() {
-    alert("back");
+    if (step > 1) setStep(step - 1);
   }
 
   function handleNextStep() {
-    alert("next");
+    if (step < 3) setStep(step + 1);
   }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
 
       <p className="message">{message[step - 1]}</p>
